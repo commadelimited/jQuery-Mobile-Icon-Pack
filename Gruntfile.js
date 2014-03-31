6b0b02904d3bc82f30912b77dd244b0f93f80ac4
andy matthews <andy@commadelimited.com>
2014-03-30 22:05:46 -0500

    Implement convert and cleanup tasks for Builder integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
             dist: {
                 options: {
                     trace: true,
-                    style: 'expanded'
+                    style: 'compact'
                 },
                 files: {
                     'dist/jqm-icon-pack-fa.css': 'dist/jqm-icon-pack-fa.scss'
@@ -53,6 +53,50 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-grunticon');
-    grunt.registerTask('default', ['grunticon', 'sass']);
+    grunt.registerTask('default', ['grunticon', 'sass', 'prepare-for-builder', 'cleanup']);
+    grunt.registerTask('prepare-for-builder', 'Prepare Icon Pack for Builder import', function() {
+        console.log('Beginning conversion of CSS');
 
+        // Open dist/jqm-icon-pack-fa.css
+        var fs = require('fs'),
+            finalArr = [],
+            inFile = __dirname + '/dist/jqm-icon-pack-fa.css',
+            outFile = __dirname + '/dist/jqm-icon-pack-fa-builder.css',
+            file_contents,
+            tmpStr;
+
+        // make sure CSS file exists
+        if (!fs.existsSync(inFile)) return false;
+
+        // read the file
+        file_contents = fs.readFileSync(inFile).toString().split('\n\n');
+
+        // Loop over file contents
+        file_contents.forEach(function(el, i){
+            // Copy any declaration beginning with `.ui-icon-` into memory
+            if (el.match(/^\.ui-icon/)) {
+                // convert declaration from `.ui-icon-<icon>:after` to `li .<icon>`
+                // also change color from white to black
+                tmpStr = el.replace('.ui-icon-','li \.').replace(':after', '').replace('ffffff', '000000');
+                finalArr.push(tmpStr);
+            }
+        });
+
+        // save as file `jqm-icon-pack-fa-builder.css`
+        fs.writeFileSync(outFile, finalArr.join('\n'));
+
+        console.log('CSS conversion complete');
+    });
+    grunt.registerTask('cleanup', 'Assorted project cleanup', function() {
+        // delete unwanted dist/* files
+        var fs = require('fs'),
+            filesToDelete = ['index.html', 'grunticon.loader.txt', 'icons.data.png.css', 'icons.fallback.css'];
+
+        filesToDelete.forEach(function(file, i){
+            fs.unlink(__dirname + '/dist/' + file, function(error){
+                // pass
+            });
+        });
+        console.log('Cleanup complete');
+    });
 };
